c4e9bfd981912fc4ae63488dfb43caf1bc2169f9
sarojdhakal307 <dhakal.saroj@techyatralabs.com>
2026-05-23 00:37:03 +0545

    feat: Initialize iOS project structure for outlinetask

--- android/app/src/main/java/com/outlinetask/MainApplication.kt
+++ android/app/src/main/java/com/outlinetask/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.outlinetasks
+package com.outlinetask
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/outlinetask/AppDelegate.swift
+++ ios/outlinetask/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "outlinetasks",
+      withModuleName: "outlinetask",
       in: window,
       launchOptions: launchOptions
     )
